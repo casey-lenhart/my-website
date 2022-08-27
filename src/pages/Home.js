026526b2adda0e0acb3bbd41e308ac671a1c78dc
casey-lenhart <110734213+casey-lenhart@users.noreply.github.com>
2022-08-27 11:15:05 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,10 +15,10 @@ const Home = () => {
       {width >= 1023 ?     <div className="hero-body"><div style={{ fontSize: width < 1280 ? '2rem' : '3rem' }}>
         <div className="columns is-flex is-align-items-center">
             <div className="column is-2"></div>
-            <div className="column is-2 is-flex is-align-items-center">
+            <div className="column is-4 is-flex is-align-items-center">
                 <img src="https://raw.githubusercontent.com/casey-lenhart/casey-lenhart.github.io/main/public/Screen Shot 2022-08-20 at 11.49.40 AM.png" />
             </div>
-            <div className="column is-6 pl-5">
+            <div className="column is-4 pl-5">
                 <b>Casey Lenhart</b>
                 <div className='is-size-4'>
                 {description}
